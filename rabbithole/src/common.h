89e3362a8f44a8d94cdb8d8c20c5d89b511ff58f
Nikola Djukic <dekanac94@gmail.com>
2022-09-19 15:09:18 +0200

    [FIX] Fix a probable disaster. My bro @tinyluc1d found this little bitch.

--- rabbithole/src/common.h
+++ rabbithole/src/common.h
@@ -25,7 +25,7 @@ private: \
 #define NonCopyableAndMovable(className) \
 public: \
 	className(const className&) = delete; \
-	className operator= (const className&) = delete; \
+	className& operator= (const className&) = delete; \
 	className(className&&) = delete; \
 	className& operator= (className&&) = delete
 
